3f292a39cf3ada20a9f5a14d3065b08df538c797
Ivan Belokobylskiy <belokobylskij@gmail.com>
2020-08-06 11:43:47 +0300

    Update requirements

--- code/flats/urls.py
+++ code/flats/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import re_path
 from django.contrib import admin
 
 from .views import IndexView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', IndexView.as_view())
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', IndexView.as_view())
 ]
